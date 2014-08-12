1279e577d5149f6c010e49ba7442d1599f5169a2
Max Johenneken <max.johenneken@anderscore.com>
2014-08-12 14:03:20 +0200

    Added Constants Class

---
+++ src/com/anderscore/simpleandroidchat/Constants.java
@@ -0,0 +1,10 @@
+package com.anderscore.simpleandroidchat;
+
+public final class Constants {
+	
+	public static final String HOST = "max-johenneken.de";
+	public static final String USER = "test1";
+	public static final String PASSWORD = "test1";
+	public static final Integer PORT = 5222;
+	
+}
