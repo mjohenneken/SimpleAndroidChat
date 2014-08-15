090fed764c56994ea4f3b5e5643f173a2365577d
Max Johenneken <max.johenneken@anderscore.com>
2014-08-15 15:08:50 +0200

    implemented ContactListAdapter

--- src/com/anderscore/simpleandroidchat/ContactListAdapter.java
+++ src/com/anderscore/simpleandroidchat/ContactListAdapter.java
@@ -2,18 +2,24 @@ package com.anderscore.simpleandroidchat;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.zip.Inflater;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 public class ContactListAdapter extends BaseAdapter {
 
 	Context context;
 	List<Contact> contacts = new ArrayList<Contact>();
 	
-	
+	static class ViewHolder {
+		TextView tViewUser;
+	}
+
 	@Override
 	public int getCount() {
 		return contacts.size();
@@ -21,7 +27,7 @@ public class ContactListAdapter extends BaseAdapter {
 
 	@Override
 	public Contact getItem(int position) {
-		
+
 		return contacts.get(position);
 	}
 
@@ -32,9 +38,27 @@ public class ContactListAdapter extends BaseAdapter {
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
+		View rowView = convertView;
+		if(rowView==null){
+		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		rowView = inflater.inflate(R.layout.row_item_contact_list, parent, false);
+		ViewHolder viewHolder = new ViewHolder();
+		viewHolder.tViewUser = (TextView) rowView.findViewById(R.id.tViewUser);
+		rowView.setTag(viewHolder);
+		}
+		ViewHolder holder = (ViewHolder) rowView.getTag();
 		
+		//Data
+		holder.tViewUser.setText(contacts.get(position).getName());
 		
-		return null;
+		if (contacts.get(position).isOnline()) {
+			holder.tViewUser.setCompoundDrawables(null, null, context.getResources().getDrawable(R.drawable.circle_green), null);
+		}
+		else {
+			holder.tViewUser.setCompoundDrawables(null, null, context.getResources().getDrawable(R.drawable.circle_gray), null);			
+		}
+
+		return rowView;
 	}
 
 }
