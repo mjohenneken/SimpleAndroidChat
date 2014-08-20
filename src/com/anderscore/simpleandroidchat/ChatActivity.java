b8a09d25fa0249b281042a8075faa60dcc473869
ploka <patrick.loka@anderscore.com>
2014-08-20 16:21:08 +0200

    Prepared UI Parts

--- src/com/anderscore/simpleandroidchat/ChatActivity.java
+++ src/com/anderscore/simpleandroidchat/ChatActivity.java
@@ -1,90 +1,62 @@
 package com.anderscore.simpleandroidchat;
 
-import com.anderscore.simpleandroidchat.Constants.Extra;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
 
+/**
+ * 
+ * @author mjohenneken
+ *	Attribut list, listadapter, etextChatMessage, btnSendMessage, Contact
+ */
+/**
+ * @author paloka
+ *
+ */
 public class ChatActivity extends AbstractActivity {
+	
 
-	ListView list;
-	ChatListAdapter listAdapter;
-	EditText eTextChatMessage;
-	Button btnSendMessage;
-
-	Contact contact;
-
+	/**
+	 * display up true, liste finden und setzen, andere views holen, listadapter instanz
+	 * 
+	 * click listeer: clear, new chatmsg, sendMsg, updateList, InputMethodManager, Intent mit Contact Extra
+	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		setContentView(R.layout.activity_chat_list);
-		getActionBar().setDisplayHomeAsUpEnabled(true);
-
-		list = (ListView) findViewById(android.R.id.list);
-		list.setEmptyView(findViewById(android.R.id.empty));
-		eTextChatMessage = (EditText) findViewById(R.id.eTextChatMessage);
-		btnSendMessage = (Button) findViewById(R.id.btnSendMessage);
-
-		listAdapter = new ChatListAdapter(this);
-		list.setAdapter(listAdapter);
-		btnSendMessage.setOnClickListener(new Button.OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				String msgText = eTextChatMessage.getText().toString();
-				if (msgText != "") {
-					eTextChatMessage.getText().clear();
-					ChatMsg msg = new ChatMsg(contact, false, msgText);
-					ChatMsg showMsg = getService().sendMessage(msg);
-					listAdapter.updateList(showMsg);
-
-					InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
-					imm.hideSoftInputFromWindow(eTextChatMessage.getWindowToken(), 0);
-				}
-			}
-		});
-
-		Intent startIntent = getIntent();
-		contact = new Contact(startIntent.getStringExtra(Extra.USER), startIntent.getBooleanExtra(Extra.IS_ONLINE, false));
-
+		//TODO		
 		super.onCreate(savedInstanceState);
 	}
 
+	/**
+	 * navigate up
+	 */
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			NavUtils.navigateUpFromSameTask(this);
-			return true;
-
-		default:
-			return super.onOptionsItemSelected(item);
-		}
+		//TODO
+		return super.onOptionsItemSelected(item);
 	}
 
-	@Override
-	void onServiceAvailable() {
-		// TODO get Messages
-
-	}
-
-	@Override
-	void notifyContact(Contact contact) {
-		// TODO
-	}
-
-	@Override
-	void notifyMsg(ChatMsg msg) {
-		if (msg.getContact().getUser().equals(contact.getUser())) {
-			listAdapter.updateList(msg);
-		}
-
-	}
+//	
+//	/**
+//	 * get messages
+//	 */
+//	@Override
+//	void onServiceAvailable() {
+//		// TODO
+//
+//	}
+//
+//	@Override
+//	void notifyContact(Contact contact) {
+//		//do nothing
+//	}
+//
+//	/**
+//	 *  listadapter update bei gleicher contact Id
+//	 * 
+//	 */
+//	@Override
+//	void notifyMsg(ChatMsg msg) {	
+//
+//	}
 
 }
--- src/com/anderscore/simpleandroidchat/ContactListAdapter.java
+++ src/com/anderscore/simpleandroidchat/ContactListAdapter.java
@@ -1,72 +1,17 @@
 package com.anderscore.simpleandroidchat;
 
-import java.util.ArrayList;
-import java.util.List;
 
-import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.TextView;
+/**
+ * 
+ * @author mjohenneken
+ * Attribut context, contacts
+ *	ids R.id.tViewUser
+ *
+ */
+public class ContactListAdapter {
 
-public class ContactListAdapter extends BaseAdapter {
-
-	Context context;
-	List<Contact> contacts = new ArrayList<Contact>();
 	
-	static class ViewHolder {
-		TextView tViewUser;
-	}
 	
-	public ContactListAdapter(Context context) {
-		this.context = context;
-	}
-
-	@Override
-	public int getCount() {
-		return contacts.size();
-	}
-
-	@Override
-	public Contact getItem(int position) {
-
-		return contacts.get(position);
-	}
-
-	@Override
-	public long getItemId(int position) {
-		return position;
-	}
-
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		View rowView = convertView;
-		if(rowView==null){
-		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		rowView = inflater.inflate(R.layout.row_item_contact_list, parent, false);
-		ViewHolder viewHolder = new ViewHolder();
-		viewHolder.tViewUser = (TextView) rowView.findViewById(R.id.tViewUser);
-		rowView.setTag(viewHolder);
-		}
-		ViewHolder holder = (ViewHolder) rowView.getTag();
-		
-		//Data
-		holder.tViewUser.setText(contacts.get(position).getUser());
-		
-		if (contacts.get(position).isOnline()) {
-			holder.tViewUser.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.circle_green, 0);
-		}
-		else {
-			holder.tViewUser.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.circle_gray, 0);		
-		}
-
-		return rowView;
-	}
 	
-	public void updateList(ArrayList<Contact> contacts) {
-		this.contacts = contacts;
-		notifyDataSetChanged();
-	}
 
 }
